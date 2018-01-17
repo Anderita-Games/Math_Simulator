7978bf4873bcf5e84c32f72b1e28b878d1d5d9e8
Andrew Friedman <andrewfriedman01@gmail.com>
2018-01-16 20:04:28 -0500

    Added App
    This is the first version of Math Simulator that GitHub has ever seen!

---
+++ Assets/Script/Game.js
@@ -0,0 +1,51 @@
+﻿#pragma strict
+var Score_Text : UnityEngine.UI.Text;
+var HighScore : UnityEngine.UI.Text;
+var Question : UnityEngine.UI.Text;
+var Answer_Text : UnityEngine.UI.Text;
+
+var Score : int;
+var Number_1 : float;
+var Number_2 : float;
+var Answer : float;
+
+var Button_Submit : GameObject;
+var Button_Restart : GameObject;
+
+function Start () {
+	Equation_Creator();
+	Button_Restart.SetActive(false);
+}
+
+function Update () {
+	if (Score > PlayerPrefs.GetInt("Highscore")) {
+		PlayerPrefs.SetInt("Highscore", Score);
+	}
+
+	Score_Text.text = "Score: " + Score.ToString();
+	HighScore.text = "Highscore: " + PlayerPrefs.GetInt("Highscore").ToString();
+}
+
+function Equation_Creator () {
+	Number_1 = Random.Range(1, 100);
+	Number_2 = Random.Range(1, 100);
+	Answer = Number_1 + Number_2;
+	Question.text = "What does " + Number_1.ToString() + " + " + Number_2.ToString() + " equal?";
+}
+
+function Submit_Answer () {
+	if (Answer_Text.text == Answer.ToString()) {
+		Score++;
+		Equation_Creator();
+	}else {
+		Question.text = "You got the wrong answer! The answer was " + Answer.ToString();
+		Button_Restart.SetActive(true);
+		Button_Submit.SetActive(false);
+	}
+	Debug.Log(Answer_Text);
+	Debug.Log(Answer.ToString());
+}
+
+function Restart () {
+	Application.LoadLevel("Game");
+}
---
+++ Assets/Script/MainMenu.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+var HighScore : UnityEngine.UI.Text;
+
+function Update () {
+	HighScore.text = "Highscore: " + PlayerPrefs.GetInt("Highscore").ToString();
+}
+
+function StartGame () {
+	Application.LoadLevel("Game");
+}
+
+/* 
+This is probably the shortest code I have ever written for Unity!
+It is a fucking Main Menu after all :)
+7/25/17 1:28 AM
+*/
